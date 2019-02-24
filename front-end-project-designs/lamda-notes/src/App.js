9fef26683f7af2d47e4b56019a32400f820b38e0
Leila <nb.leila10@gmail.com>
2019-02-24 17:39:20 -0600

    Add create note functionality

--- front-end-project-designs/lamda-notes/src/App.js
+++ front-end-project-designs/lamda-notes/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react"
-import { Route } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 
 import Navbar from "./components/Navbar"
 import NotesList from "./containers/NotesList"
 import NoteView from "./containers/NoteView"
+import NoteForm from "./containers/NoteForm"
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Route path="/note/:id" component={NoteView} />
-        <Route exact path="/" component={NotesList} />
+        <Switch>
+          <Route
+            path="/note/add"
+            render={props => <NoteForm {...props} actionType="Add" />}
+          />
+          <Route path="/note/:id" component={NoteView} />
+          <Route exact path="/" component={NotesList} />
+        </Switch>
       </div>
     )
   }
--- front-end-project-designs/lamda-notes/src/store/actions/index.js
+++ front-end-project-designs/lamda-notes/src/store/actions/index.js
@@ -6,6 +6,9 @@ export const ERROR_NOTES = "ERROR_NOTES"
 export const FETCHING_NOTE = "FETCHING_NOTE"
 export const FETCHED_NOTE = "FETCHED_NOTE"
 export const ERROR_NOTE = "ERROR_NOTE"
+export const CREATING = "CREATING"
+export const CREATING_ERROR = "CREATING_ERROR"
+export const CREATED = "CREATED"
 
 export function fetchNotes() {
   return dispatch => {
@@ -13,13 +16,42 @@ export function fetchNotes() {
     axios
       .get("https://fe-notes.herokuapp.com/note/get/all")
       .then(response => {
-        console.log(response)
         dispatch({ type: FETCHED_NOTES, payload: response.data })
       })
       .catch(error => {
-        dispatch({ type: ERROR_NOTES, payload: "Can't fetch the cards!" })
+        dispatch({ type: ERROR_NOTES, payload: "Can't fetch the notes!" })
       })
   }
 }
 
-export function fetchNote() {}
+export function fetchNote(id) {
+  return dispatch => {
+    dispatch({ type: FETCHING_NOTE })
+    axios
+      .get(`https://fe-notes.herokuapp.com/note/get/${id}`)
+      .then(response => {
+        dispatch({ type: FETCHED_NOTE, payload: response.data })
+      })
+      .catch(error => {
+        dispatch({ type: ERROR_NOTE, payload: "Can't fetch the note!" })
+      })
+  }
+}
+
+export function addNote(newNote, history) {
+  return dispatch => {
+    dispatch({ type: CREATING })
+    axios
+      .post("https://fe-notes.herokuapp.com/note/create", newNote)
+      .then(response => {
+        dispatch({ type: CREATED })
+        history.push("/")
+      })
+      .catch(error => {
+        dispatch({
+          type: CREATING_ERROR,
+          payload: "Can't Create the new Note!"
+        })
+      })
+  }
+}
