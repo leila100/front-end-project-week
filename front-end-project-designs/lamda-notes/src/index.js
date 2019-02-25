081d26f4a1a390ee8e0218742274ab4f5ecf852f
Leila <nb.leila10@gmail.com>
2019-02-24 20:11:47 -0600

    Add delete note functionality

--- front-end-project-designs/lamda-notes/src/index.js
+++ front-end-project-designs/lamda-notes/src/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
 import thunk from "redux-thunk"
 import logger from "redux-logger"
 import { createStore, applyMiddleware, compose } from "redux"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router } from "react-router-dom"
 import rootReducer from "./store/reducers"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./index.css"
 
 import App from "./App"
 
